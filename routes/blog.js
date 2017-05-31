a2f30b1b98d3687fba7a337a141822ebed94f94c
4husky <husky.uit.workshop@gmail.com>
2017-05-31 21:58:14 +0700

    cap nhat cac bai post

--- routes/blog.js
+++ routes/blog.js
@@ -1,14 +1,25 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var router = express.Router();
 
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
 /* GET home page. */
 
-router.get('/', function(req, res) {
-  res.render('blog/index', { title: 'Express' });
+router.get('/:id', function(req, res) {
+  var id = req.params.id;
+  var url = 'blog/page_'+ String(id)+'/index';	
+  res.render(url, { myurl: url });
 });
 
-router.get('/post/1', function(req, res) {
-  res.render('blog/Post/Post_1/post_1', { title: 'Express' });
+router.get('/post/:id', function(req, res) {
+  var id = req.params.id;
+  var url = 'blog/posts/post_'+ String(id) +'/index';
+  res.render(url, { myurl: url });
 });
 
+var content;
+router.post('/post/:id',urlencodedParser,function(req,res){
+	content = req.body;
+	var cmt = content[0]; 
+});
 module.exports = router;
